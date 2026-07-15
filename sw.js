99847e0f0fd21e1634c8cc8c51fce3abadfd0ac5
funkysword-1 <pranav.ganapathiyadan@gmail.com>
2026-07-15 02:16:55 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daybook-v18";
+const CACHE = "daybook-v19";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
